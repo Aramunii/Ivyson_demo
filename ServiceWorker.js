9388937e1e9e099a09b29608d0c1c0ca3b3205b9
Maer costa <39601135+Aramunii@users.noreply.github.com>
2024-04-26 13:56:45 -0300

    a

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Ivyson_Demo-1.0";
+const contentToCache = [
+    "Build/teste.loader.js",
+    "Build/teste.framework.js",
+    "Build/teste.data",
+    "Build/teste.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
